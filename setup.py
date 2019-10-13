1e21e56c9e595b3abe714e2ee935698a7c521629
jackbkennedy <kennej14@tcd.ie>
2019-10-13 17:32:17 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="onelab_api-jackbkennedy",
+    name="onelab-jackbkennedy",
     version="0.0.1",
     author="Jack Kennedy",
     author_email="jackbkennedy@gmail.com",
     description="A python package to use gmsh and ONELAB via their original python APIs",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jackbkennedy/onelab_api",
+    url="https://github.com/jackbkennedy/onelab",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
