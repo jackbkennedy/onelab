1a0cd9b7987af2b41a1dd9547266a15217aaa7ed
jackbkennedy <kennej14@tcd.ie>
2020-01-26 17:03:20 +0000

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="onelab",
-    version="0.8",
+    version="0.9",
     author="Jack Kennedy",
     author_email="jackbkennedy@gmail.com",
     description="A python package to use gmsh and ONELAB via their original python APIs",
